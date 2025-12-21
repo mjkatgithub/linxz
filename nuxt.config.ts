3c9eec5204a89e381ccf9348c8422063d5b17109
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-12-21 19:45:02 +0100

    Update Nuxt config and dependencies
    - Enhance Nuxt devtools with timeline and component inspector.
    - Add Pinia store configuration for improved state management.
    - Include crypto-js and its type definitions and ensure compatibility.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@prisma/nuxt', '@pinia/nuxt'],
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true
+    },
+    // Explizite Konfiguration fuer Pinia-Integration
+    // in den DevTools
+    componentInspector: {
+      enabled: true
+    }
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@prisma/nuxt',
+    '@pinia/nuxt'
+  ],
+  pinia: {
+    storesDirs: ['./stores'],
+    autoImports: ['defineStore', 'storeToRefs']
+  },
   css: [
     'vuetify/styles',
     '@mdi/font/css/materialdesignicons.css',
